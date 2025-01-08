b70790ca542e1e5b8e62ed45091bd9ebfd015b79
jacobschmitt23 <159497424+jacobschmitt23@users.noreply.github.com>
2025-01-07 20:57:45 -0500

    Create admin.py

---
+++ model/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from model.models import *
+
+# Register your models here.
+admin.site.register(individualResultTable)
+admin.site.register(aggregateDataTable)
+admin.site.register(individualTable)
+admin.site.register(doctorLink)
+admin.site.register(latency)
